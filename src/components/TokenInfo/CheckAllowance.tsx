9699feb7946454531ef1f3e9af52582b82fa45e7
sriramsenthill <sriram.senthilnathan@gmail.com>
2025-02-10 17:20:43 +0530

    fix: check allowance

--- src/components/TokenInfo/CheckAllowance.tsx
+++ src/components/TokenInfo/CheckAllowance.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import { useState } from "react";
-import { Box, Input, Text, Heading, Stack } from "@chakra-ui/react";
-import { toaster } from "@/components/ui/toaster";
-import Button from "@/components/ui/button";
 import { useTokenOperations } from "@/hooks/useTokenOperations";
+import { toaster } from "../ui/toaster";
+import Button from "../ui/button";
 
-
-const CheckAllowance = () => {
+export default function CheckAllowance() {
     const [loading, setLoading] = useState<boolean>(false);
     const [owner, setOwner] = useState<string>("");
     const [spender, setSpender] = useState<string>("");
@@ -13,12 +13,11 @@ const CheckAllowance = () => {
 
     const { checkAllowance } = useTokenOperations();
 
-    const allowance = async () => {
+    const handleAllowanceCheck = async () => {
         if (owner.trim() === "" || spender.trim() === "") {
             toaster.create({
                 title: "Warning",
                 description: "Both owner and spender wallet address is required.",
-                type: "info",
             });
             return;
         }
@@ -27,20 +26,16 @@ const CheckAllowance = () => {
 
         try {
             const result = await checkAllowance(owner, spender);
-
             setAmount(result.toString());
-
             toaster.create({
                 title: "Success",
                 description: "Checked allowance successfully.",
-                type: "success",
             });
         } catch (error) {
             console.error("Error fetching allowance:", error);
             toaster.create({
                 title: "Error",
                 description: "Failed to fetch allowance. Please try again.",
-                type: "error",
             });
         } finally {
             setLoading(false);
@@ -48,69 +43,75 @@ const CheckAllowance = () => {
     };
 
     return (
-        <Box
-            p={8}
-            borderWidth={2}
-            borderRadius="lg"
-            boxShadow="sm"
-            bg="gray.50"
-            mx="auto"
-            mt={8}
-        >
-            <Heading fontSize="xl" mb={4} color="black" fontWeight="semibold" textAlign="center">
-                Check Allowance
-            </Heading>
-            <Stack gap={4}>
-                <Box>
-                    <Text fontSize="sm" fontWeight="semibold" color="gray.600" mb={1}>
-                        From Address
-                    </Text>
-                    <Input
-                        color="black"
-                        pl="4"
-                        placeholder="Enter from address"
-                        value={owner}
-                        onChange={(e) => setOwner(e.target.value)}
-                        bg="gray.100"
-                        borderColor="gray.300"
-                        _focus={{ borderColor: "teal.500", boxShadow: "0 0 0 1px #319795" }}
-                        _hover={{ borderColor: "teal.400" }}
-                    />
-                </Box>
-                <Box>
-                    <Text fontSize="sm" fontWeight="semibold" color="gray.600" mb={1}>
-                        To Address
-                    </Text>
-                    <Input
-                        color="black"
-                        pl="4"
-                        placeholder="Enter to address"
-                        value={spender}
-                        onChange={(e) => setSpender(e.target.value)}
-                        bg="gray.100"
-                        borderColor="gray.300"
-                        _focus={{ borderColor: "teal.500", boxShadow: "0 0 0 1px #319795" }}
-                        _hover={{ borderColor: "teal.400" }}
-                    />
-                </Box>
-                <Button
-                    bg="teal.500"
-                    colorScheme="teal"
-                    onClick={allowance}
-                    loading={loading}
-                    loadingText="Checking..."
-                    _hover={{ bg: "teal.400" }}
-                    _active={{ bg: "teal.600" }}
-                >
-                    Check Allowance
-                </Button>
-                <Text fontSize="sm" fontWeight="semibold" color="gray.700" textAlign="center">
-                    Allowance: {amount} FLR
-                </Text>
-            </Stack>
-        </Box>
+        <div className="flex flex-col gap-4 p-3 bg-white/50 rounded-2xl ">
+            <div className="relative flex flex-col gap-4">
+                {/* From Address Section */}
+                <div className="flex flex-col gap-2 rounded-2xl bg-white p-4">
+                    <div className="flex justify-between">
+                        <div className="flex gap-3">
+                            <span className="font-satoshi text-custom-gray text-xs font-bold">From</span>
+                        </div>
+                    </div>
+                    <div className="flex h-6 justify-between">
+                        <input
+                            className="w-full outline-none border-none bg-transparent placeholder:text-custom-gray text-base sm:text-xl font-bold"
+                            type="text"
+                            placeholder="Enter from address"
+                            value={owner}
+                            onChange={(e) => setOwner(e.target.value)}
+                        />
+                    </div>
+                </div>
+
+
+                {/* To Address Section */}
+                <div className="flex flex-col gap-2 rounded-2xl bg-white p-4">
+                    <div className="flex justify-between">
+                        <div className="flex gap-3">
+                            <span className="font-satoshi text-custom-gray text-xs font-bold">To</span>
+                        </div>
+                    </div>
+                    <div className="flex h-6 justify-between">
+                        <input
+                            className="w-full outline-none border-none bg-transparent placeholder:text-custom-gray text-base sm:text-xl font-bold"
+                            type="text"
+                            placeholder="Enter to address"
+                            value={spender}
+                            onChange={(e) => setSpender(e.target.value)}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            {/* Details Section */}
+            <div className="flex cursor-pointer flex-col gap-3 text-custom-gray rounded-2xl bg-white/50 px-4 pb-3 pt-4 transition-[background-color] hover:bg-white">
+                <span className="font-satoshi text-dark-grey text-xs font-bold">Details</span>
+                <div>
+                    <div className="flex items-center justify-between">
+                        <span className="font-satoshi text-dark-grey text-xs font-medium">Allowance</span>
+                        <div className="flex gap-5 py-1">
+                            <span className="font-satoshi text-dark-grey text-sm font-medium">
+                                {amount} FLR
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Check Allowance Button */}
+            <Button
+                onClick={handleAllowanceCheck}
+                disabled={loading}
+                className={`transition-colors duration-500 font-bold min-w-32 px-6 focus:outline-none border-none h-12 rounded-2xl text-base
+                    ${loading
+                        ? 'bg-button-disabled-bg text-button-disabled-text cursor-not-allowed'
+                        : 'bg-primary text-white hover:bg-primary/90'
+                    }`}
+            >
+                <div className="flex justify-center items-center gap-3">
+                    {loading ? 'Checking...' : 'Check Allowance'}
+                </div>
+            </Button>
+        </div>
     );
-
-};
-
-export default CheckAllowance;
+}
